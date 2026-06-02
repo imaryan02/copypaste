d50b1e8eebc6c8f6e5945e81d94bbaf6a0217e4f
imaryan02 <aryan.official02@gmail.com>
2026-06-02 17:44:58 +0530

    redirected to freeseva

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/copypasteguru/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
